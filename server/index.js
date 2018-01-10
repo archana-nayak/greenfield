eaf4a5b761466a107cdf0639609046d272693abc
Kenneth <kenny.laprelle@gmail.com>
2018-01-09 17:06:09 -0800

    server hooked

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ let app = express();
 var database = require('../database/index.js');
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
-app.post('/guests', function(req, res){
+app.post('/meetups', function(req, res){
 
 });
 app.post('/users', function(req, res){
