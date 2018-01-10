46ec57b36e288c037c1ec50aa9c20ed40f47c8f5
Kenneth <kenny.laprelle@gmail.com>
2018-01-09 17:03:31 -0800

    server hooked

--- server/index.js
+++ server/index.js
@@ -7,7 +7,15 @@ let app = express();
 var database = require('../database/index.js');
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
+app.post('/guests', function(req, res){
 
+});
+app.post('/users', function(req, res){
+
+});
+app.get('/users', function(req, res){
+
+});
 
 app.listen(3000, function(){
   console.log('listening on port 3000!')
