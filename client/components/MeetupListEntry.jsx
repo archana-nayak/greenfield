375a3f45579c66a1b1779e56373134760ad28224
Kenneth <kenny.laprelle@gmail.com>
2018-01-18 08:19:06 -0800

    fixed sytling for whole entry to change color on hover

--- client/components/MeetupListEntry.jsx
+++ client/components/MeetupListEntry.jsx
@@ -6,7 +6,7 @@ import React from 'react';
        return tmp.textContent;
     }
     var desc = strip(props.event.description).slice(0,150) + '...';
-    return <div><a href={props.event.link} target="_blank"><li>{props.event.name}</li></a>
+    return <div className="mappedEvents"><a href={props.event.link} target="_blank"><li>{props.event.name}</li></a>
     <div className="eventDescription">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{desc}</div><button onClick={() => {props.seeMore(props.event.name)}}>See More</button><hr></hr></div>
   }
  export default MeetupListEntry;
